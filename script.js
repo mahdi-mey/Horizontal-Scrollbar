a5d82c1aada441a0d87e5176b8212b8b59f11179
Mahdi <mahdimey17@gmail.com>
2023-10-14 15:15:33 +0330

    added functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const progressBar = document.querySelector('.page-progress')
 
 window.addEventListener('scroll' , () => {
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop
     
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+    let scrolled = (scroll / height) * 100
+    progressBar.style.width = `${scrolled}%`
 })
